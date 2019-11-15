576dc89378b3fc14e980b1feaf18e182a95d787e
Omar Aflak <aflakomar@gmail.com>
2019-11-15 15:51:18 +0100

    ending equal sign test

--- tests/test_base64.py
+++ tests/test_base64.py
@@ -9,7 +9,7 @@ def random_string(stringLength=100):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-def test_base64_encode_characters():
+def test_characters():
     string = random_string()
     encoded = base64_encode(string)
     good = True
@@ -18,9 +18,15 @@ def test_base64_encode_characters():
             good = False
     assert good
 
-def test_base64_simple():
+def test_ending_equal():
+    string = random_string()
+    encoded = base64_encode(string)
+    substr = encoded[:-2]
+    assert '=' not in substr
+
+def test_simple():
     assert base64_encode("omar aflak") == "b21hciBhZmxhawo="
 
-def test_base64_length():
+def test_length():
     string = random_string()
     assert len(base64_encode(string)) > len(string)
