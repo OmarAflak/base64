e4e248e828aecfbfd3c1ee3ee2939d9abfd85b1c
Omar Aflak <aflakomar@gmail.com>
2019-11-15 16:04:33 +0100

    renamed tests + added multiple encode methods

--- encoder/base64.py
+++ encoder/base64.py
@@ -1,2 +1,17 @@
-def base64_encode(string):
+def base64_encode(data):
+    if type(data) is str:
+        return base64_encode_string(data)
+    elif type(data) is list:
+        return base64_encode_bytes(data)
+    return None
+
+def base64_encode_string(string):
+    data = [ord(c) for c in string]
+    return base64_encode_bytes(data)
+
+def base64_encode_bytes(data):
+    stream = ''.join(['{0:08b}'.format(n) for n in data])
+    return base64_encode_binary(stream)
+
+def base64_encode_binary(stream):
     pass
--- tests/test_base64.py
+++ tests/test_base64.py
@@ -9,7 +9,7 @@ def random_string(stringLength=100):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-def test_characters():
+def test_encode_characters():
     string = random_string()
     encoded = base64_encode(string)
     good = True
@@ -18,15 +18,15 @@ def test_characters():
             good = False
     assert good
 
-def test_ending_equal():
+def test_encode_ending_equal():
     string = random_string()
     encoded = base64_encode(string)
     substr = encoded[:-2]
     assert '=' not in substr
 
-def test_simple():
+def test_encode_simple():
     assert base64_encode("omar aflak") == "b21hciBhZmxhawo="
 
-def test_length():
+def test_encode_length():
     string = random_string()
     assert len(base64_encode(string)) > len(string)
