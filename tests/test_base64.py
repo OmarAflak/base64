0a60753682da15064c2c7839365e1f64f578ed4f
Omar Aflak <aflakomar@gmail.com>
2019-11-15 15:49:08 +0100

    test size

--- tests/test_base64.py
+++ tests/test_base64.py
@@ -1,11 +1,26 @@
+import random
+import string
+
 from encoder.base64 import base64_encode
 
 character_set = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+=')
 
+def random_string(stringLength=100):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
 def test_base64_encode_characters():
-    encoded = base64_encode("omar aflak")
+    string = random_string()
+    encoded = base64_encode(string)
     good = True
     for c in encoded:
         if c not in character_set:
             good = False
     assert good
+
+def test_base64_simple():
+    assert base64_encode("omar aflak") == "b21hciBhZmxhawo="
+
+def test_base64_length():
+    string = random_string()
+    assert len(base64_encode(string)) > len(string)
