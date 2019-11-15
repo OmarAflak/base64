412b4e92127a6fce5c9ebc2cf4741629790f60b3
Omar Aflak <aflakomar@gmail.com>
2019-11-15 16:45:47 +0100

    fixed trailing zeros issue

--- encoder/base64.py
+++ encoder/base64.py
@@ -1,3 +1,16 @@
+def base64_char_mapping(n):
+    if 0 <= n <= 25:
+        return chr(n + ord('A'))
+    elif 26 <= n <= 51:
+        return chr(n + ord('a') - 26)
+    elif 52 <= n <= 61:
+        return chr(n + ord('0') - 52)
+    elif n == 62:
+        return '+'
+    elif n == 63:
+        return '/'
+    return None
+
 def base64_encode(data):
     if type(data) is str:
         return base64_encode_string(data)
@@ -14,4 +27,11 @@ def base64_encode_bytes(data):
     return base64_encode_binary(stream)
 
 def base64_encode_binary(stream):
-    pass
+    size = len(stream) // 8
+    trailing_zeros = 3 - size % 3
+    stream += '000000' * trailing_zeros
+    new_stream = [stream[i:i+6] for i in range(0, len(stream), 6)]
+    new_data = [int(binary, 2) for binary in new_stream]
+    new_chars = [base64_char_mapping(n) for n in new_data[:-trailing_zeros]]
+    new_chars.extend(['='] * trailing_zeros)
+    return ''.join(new_chars)
